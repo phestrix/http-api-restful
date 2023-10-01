29480bcf9a92ffdd5482d9948c0e047ea4a65b66
matri <matri@NITRO5>
2023-10-01 20:49:08 +0700

    add annotation

--- src/main/java/ru/phestrix/httpapirestful/customer/CustomerRepository.java
+++ src/main/java/ru/phestrix/httpapirestful/customer/CustomerRepository.java
@@ -1,6 +1,8 @@
 package ru.phestrix.httpapirestful.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
