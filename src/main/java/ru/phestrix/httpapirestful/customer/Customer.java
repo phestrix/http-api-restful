1cf2faa8a3574203e71fc005d22a4802913200ac
matri <matri@NITRO5>
2023-10-01 20:21:12 +0700

    added customer entity

---
+++ src/main/java/ru/phestrix/httpapirestful/customer/Customer.java
@@ -0,0 +1,68 @@
+package ru.phestrix.httpapirestful.customer;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_sequence"
+    )
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Customer(Long id, String name, String email, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Customer() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+}
