4e7b755f95cdd9b0ffa0899f94e4e4136b4a1ae4
matri <matri@NITRO5>
2023-10-01 20:27:02 +0700

    added customer repository

---
+++ src/main/java/ru/phestrix/httpapirestful/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package ru.phestrix.httpapirestful.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
